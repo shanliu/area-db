0ebfd9db3acb1bb94d2fdbcec830d33b6b24fb08
liushan <shan.liu@msn.com>
2023-11-07 11:20:10 +0800

    ..

--- src/dao/utils.rs
+++ src/dao/utils.rs
@@ -1,11 +1,14 @@
-const CLEAR_WORD: &[&str] = &[
-    "-",
-    "社区居委会",
-    "居委会",
-    "片区街道",
-    "街道办",
-    "委会",
-    "民委员会",
+const CLEAR_WORD: &[(&str, &str)] = &[
+    ("-", "-"),
+    ("社区居委会", "居委会"),
+    ("居委会", "居委会"),
+    ("片区街道", "片区街道"),
+    ("街道办", "办"),
+    ("村委会", "委会"),
+    ("村村", "村"),
+    ("民委员会", "民委员会"),
+    ("省直辖县级行政区划", "级行政区划"),
+    ("省直辖市级行政区划", "级行政区划"),
 ];
 const BAD_WORD: &[&str] = &[
     "-",
@@ -16,9 +19,6 @@ const BAD_WORD: &[&str] = &[
     "特别行政区",
     "自治区",
     "直辖县级行政区划",
-    "村委会",
-    "居委会",
-    "省",
     "片区街道",
     "街道办",
 ];
@@ -28,11 +28,14 @@ pub(crate) fn key_word_clear(name: &str) -> &str {
     if name.chars().count() <= 2 {
         return name;
     }
-    for tmps in [BAD_WORD, CLEAR_WORD] {
-        for tmp in tmps {
-            if name.ends_with(tmp) {
-                return name.trim_end_matches(tmp);
-            }
+    for tmp in BAD_WORD {
+        if name.ends_with(tmp) {
+            return name.trim_end_matches(tmp);
+        }
+    }
+    for tmp in CLEAR_WORD {
+        if name.ends_with(tmp.0) {
+            return name.trim_end_matches(tmp.1);
         }
     }
     name
@@ -41,8 +44,8 @@ pub(crate) fn key_word_clear(name: &str) -> &str {
 pub(crate) fn name_clear(name: &str) -> String {
     let mut tmp = name.trim();
     for tcc in CLEAR_WORD {
-        if tmp.ends_with(tcc) {
-            tmp = tmp.trim_end_matches(tcc);
+        if tmp.ends_with(tcc.0) {
+            tmp = tmp.trim_end_matches(tcc.1);
         }
     }
     tmp.to_owned()
